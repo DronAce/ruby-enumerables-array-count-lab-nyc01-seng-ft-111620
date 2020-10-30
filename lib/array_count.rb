44fb269cec0bcdc988546f25bace1e05cf063a41
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-30 19:42:51 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,6 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  b = 0
-  b = array.count
-  b
+  array.count
 end
 
 def count_empty_strings(array)
